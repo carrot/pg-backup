b8ec6b83b925ebbdf1f7e2d47bb837bc7e43b4f5
Jeff Escalante <escalantejeff@gmail.com>
2016-10-17 19:09:55 -0400

    adjustments, cli working

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 const PgBackup = require('..')
 const program = require('commander')
 const Configstore = require('configstore')
@@ -8,40 +10,62 @@ const store = new Configstore(pkg.name)
 
 program
   .version(pkg.version)
-  .option('-a --account-id', 'Configure B2 account id')
-  .option('-k --application-key', 'Configure B2 application key')
-  .option('-b --bucket-id', 'Configure B2 bucket id')
-  .option('-n --name', 'Postgres database name to back up')
-  .option('-s --schedule', 'Add a cron task for recurring backups')
+  .option('-a --account-id [value]', 'Configure B2 account id')
+  .option('-k --application-key [value]', 'Configure B2 application key')
+  .option('-b --bucket-id [value]', 'Configure B2 bucket id')
+  .option('-n --dbname [value]', 'Postgres database name to back up')
+  .option('-s --schedule [value]', 'Add a cron task for recurring backups')
   .parse(process.argv)
 
 if (program.accountId) store.set('accountId', program.accountId)
 if (program.applicationKey) store.set('applicationKey', program.applicationKey)
 if (program.bucketId) store.set('bucketId', program.bucketId)
+const allConfig = store.all
 
-const backup = new PgBackup(store.all)
+if (Object.keys(allConfig).length < 3) {
+  errorLog('you must configure your backblaze credentials first')
+  infoLog('example:')
+  infoLog('pg-backups --account-id xxx --application-key xxx --bucket-id xxx')
+} else {
+  const backup = new PgBackup(allConfig)
 
-const name = program.name
-if (name) {
-  backup.run({ name })
-    .done(
-      () => console.log(chalk.green(`✓ ${name} backed up`)),
-      (err) => {
-        console.error(chalk.red(`× ${name} error backing up`))
-        console.error(err)
-      }
-    )
+  const name = program.dbname
+  if (name) {
+    backup.run({ name })
+      .done(
+        () => successLog(`database "${name}" backed up`),
+        (err) => {
+          errorLog('error backing up')
+          console.error(err)
+        }
+      )
+  }
+
+  const schedule = program.schedule
+  if (schedule) {
+    let timing
+    if (schedule === 'hourly') timing = '0 * * * *'
+    if (schedule === 'daily') timing = '0 0 * * *'
+    if (schedule === 'weekly') timing = '0 0 * * 0'
+    if (schedule === 'monthly') timing = '0 0 1 * *'
+
+    if (!timing) {
+      errorLog('schedule must be "hourly", "daily", "weekly", or "monthly"')
+    } else {
+      cron.add(`${timing} pg-backup ${program.dbname}`)
+        .done(() => successLog(`scheduled to back up ${schedule}`))
+    }
+  }
 }
 
-const schedule = program.schedule
-if (schedule) {
-  let timing
-  if (schedule === 'hourly') timing = '0 * * * *'
-  if (schedule === 'daily') timing = '0 0 * * *'
-  if (schedule === 'weekly') timing = '0 0 * * 0'
-  if (schedule === 'monthly') timing = '0 0 1 * *'
-  cron.add(`${timing} pg-backup ${program.name}`)
-    .done(() => {
-      console.log(chalk.green(`✓ scheduled to back up ${schedule}`))
-    })
+function successLog (msg) {
+  console.log(chalk.green(`✓ ${msg}`))
+}
+
+function errorLog (msg) {
+  console.error(chalk.red(`× ${msg}`))
+}
+
+function infoLog (msg) {
+  console.log(chalk.grey(`▸ ${msg}`))
 }
--- test/index.js
+++ test/index.js
@@ -6,7 +6,7 @@ test('basic', (t) => {
   const backup = new PgBackup({
     accountId: process.env.B2_ACCOUNT_ID,
     applicationKey: process.env.B2_APPLICATION_KEY,
-    bucketId: process.env.B2_BUCKET_NAME
+    bucketId: process.env.B2_BUCKET_ID
   })
 
   return backup.run({ name: 'postgres' }).then(console.log)
